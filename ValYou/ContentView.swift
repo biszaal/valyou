658cb86ac1c99cf1feb085d43da72a7039f650ac
Bishal Aryal <biszaal@yahoo.com>
2021-01-19 20:53:57 +0545

    Initial Commit

---
+++ ValYou/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ValYou
+//
+//  Created by Bishal Aryal on 19/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ValYou/ValYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValYouApp.swift
+//  ValYou
+//
+//  Created by Bishal Aryal on 19/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct ValYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
